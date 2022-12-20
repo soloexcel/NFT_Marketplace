ea8a220e6131961633cf278488dee024f52af2ba
soloexcel <solomonlekan1@gmail.com>
2022-12-20 10:32:25 -0500

    updated the component

--- frontend/components/componentsIndex.js
+++ frontend/components/componentsIndex.js
@@ -6,5 +6,7 @@ import Services from './Services/Services';
 import NFTSlider from './NFTSlider/NFTSlider';
 import Subscription from './Subscription/Subscription';
 import Title from './Title/Title';
+import Category from './Category/Category';
+import Filter from './Filter/Filter';
 
-export { Button, Footer, NavBar, HeroSection, Services, NFTSlider, Subscription, Title }
+export { Button, Footer, NavBar, HeroSection, Services, NFTSlider, Subscription, Title, Category, Filter }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
+
 import Style from '../styles/index.module.css';
-import { HeroSection, Services, NFTSlider, Subscription, Title } from '../components/componentsIndex';
+import { HeroSection, Services, NFTSlider, Subscription, Title, Category, Filter } from '../components/componentsIndex';
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Services />
       <NFTSlider />
+      <Title heading="Featured NFTs" paragraph="Discover the mmost outstanding NFTs in all topics of life."/>
+      <Filter />
       <Title heading="Browse by category" paragraph="Explore the NFTs in the most featured categories."/>
+      <Category />
       <Subscription />
-      
     </div>
   );
 };
