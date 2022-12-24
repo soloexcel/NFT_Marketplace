bd152e32d948f209efd814add3f791331f29a730
soloexcel <solomonlekan1@gmail.com>
2022-12-24 11:48:44 -0500

    Nfts by collection

---
+++ frontend/components/CardSection/Card.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { BsImages } from 'react-icons/bs';
+import Image from 'next/image';
+import Link from "next/link";
+
+import images from '../../img';
+import Style from './Card.module.css';
+
+
+const Card = ({ NFTData }) => {
+
+    const [like, setLike] = useState(true);
+    const featureArray = [1,2,3,4,5,6,7,8,9];
+
+
+
+    const likeNft = () => {
+      if (!like) {
+        setLike(true);
+      } else {
+        setLike(false);
+      }
+    };
+  
+    
+    return (
+        <div className={Style.NFTCard}>
+          {featureArray.map((el, i) => (
+            <Link href={{ pathname: "/NFT-details", query: el }}>
+              <div className={Style.NFTCard_box} key={i + 1}>
+                <div className={Style.NFTCard_box_img}>
+                  <Image
+                    src={images.nft_image_1}
+                    alt="NFT images"
+                    width={400}
+                    height={400}
+                    className={Style.NFTCard_box_img_img}
+                  />
+                </div>
+    
+                <div className={Style.NFTCard_box_update}>
+                  <div className={Style.NFTCard_box_update_left}>
+                    <div
+                      className={Style.NFTCard_box_update_left_like}
+                      onClick={() => likeNft()}
+                    >
+                      {like ? (
+                        <AiOutlineHeart />
+                      ) : (
+                        <AiFillHeart
+                          className={Style.NFTCard_box_update_left_like_icon}
+                        />
+                      )}
+                      {""} 15
+                    </div>
+                  </div>
+    
+                  <div className={Style.NFTCard_box_update_right}>
+                    <div className={Style.NFTCard_box_update_right_info}>
+                      <small>Remaining time</small>
+                      <p>3h : 15m : 20s</p>
+                    </div>
+                  </div>
+                </div>
+    
+                <div className={Style.NFTCard_box_update_details}>
+                  <div className={Style.NFTCard_box_update_details_price}>
+                    <div className={Style.NFTCard_box_update_details_price_box}>
+                      <h4>
+                        {el.name} #{el.tokenId}
+                      </h4>
+    
+                      <div
+                        className={Style.NFTCard_box_update_details_price_box_box}
+                      >
+                        <div
+                          className={Style.NFTCard_box_update_details_price_box_bid}
+                        >
+                          <small>Current Bid</small>
+                          <p>{el.price}ETH</p>
+                        </div>
+                        <div
+                          className={
+                            Style.NFTCard_box_update_details_price_box_stock
+                          }
+                        >
+                          <small>61 in stock</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={Style.NFTCard_box_update_details_category}>
+                    <BsImages />
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      );
+}
+
+export default Card
